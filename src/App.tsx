3179ed762aa88355b4674c9f79ac4713e3059f83
Taira Aikawa <tir.3t.f22@gmail.com>
2025-10-21 14:04:51 +0000

    github pages でdialog が開けない問題を解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import { Router } from './Router/Router';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <>
-      <Router />
+      <Home />
     </>
   );
 }
